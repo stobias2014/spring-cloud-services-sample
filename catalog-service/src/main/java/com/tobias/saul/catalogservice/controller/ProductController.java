f86aab241ae78740efc01250daf3d740f0d6564b
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-15 00:29:04 -0500

    added RESTful response to ProductServiceController

--- catalog-service/src/main/java/com/tobias/saul/catalogservice/controller/ProductController.java
+++ catalog-service/src/main/java/com/tobias/saul/catalogservice/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.tobias.saul.catalogservice.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +29,28 @@ public class ProductController {
 	}
 	
 	@GetMapping("/products")
-	public List<Product> allProducts() {
-		return productService.findAllProducts();
+	public CollectionModel<EntityModel<Product>> allProducts() {
+		
+		List<EntityModel<Product>> products = productService.findAllProducts()
+				.stream()
+				.map(product -> new EntityModel<Product>(product,
+						linkTo(methodOn(ProductController.class).productByCode(product.getCode())).withSelfRel(),
+						linkTo(methodOn(ProductController.class).allProducts()).withRel("products")))
+				.collect(Collectors.toList());
+		
+		return new CollectionModel<EntityModel<Product>>(products,
+				linkTo(methodOn(ProductController.class).allProducts()).withSelfRel());
 	}
 	
 	@GetMapping("/products/{code}")
-	public Product productByCode(@PathVariable("code") String code) {
-		return productService.findByProductCode(code)
+	public EntityModel<Product> productByCode(@PathVariable("code") String code) {
+		
+		Product product = productService.findByProductCode(code)
 				.orElseThrow(() -> new ProductNotFoundException("Product with code [" + code + "] does not exist"));
+		
+		return new EntityModel<Product>(product,
+				linkTo(methodOn(ProductController.class).productByCode(code)).withSelfRel(),
+				linkTo(methodOn(ProductController.class).allProducts()).withRel("products"));
 	}
 
 }
